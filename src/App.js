3778533a2bc63cce9e03e69e11fe8f5971f8d2f3
longpatrick <long.patrick1997@gmail.com>
2021-04-07 14:01:01 -0500

    add fetch call to API data from backend and update state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [state, setState] = useState({
+    data: []
+  });
+
+  const getAppData = async () => {
+    try {
+      const BASE_URL = 'http://localhost:3001/api/movies/popular';
+      const movies = await fetch(BASE_URL).then(res => res.json());
+      setState(prevState => ({
+        ...prevState, 
+        movies
+      }))
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAppData();
+  }, []); 
+
+  console.log(state);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Header
       </header>
     </div>
   );
