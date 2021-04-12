b6e639c95e94eafc109eed53d9d85259a7e04a74
longpatrick <long.patrick1997@gmail.com>
2021-04-12 13:14:01 -0500

    add embedded youtube video; add more styling for show page

--- src/Pages/MovieShow/MovieShow.js
+++ src/Pages/MovieShow/MovieShow.js
@@ -1,20 +1,40 @@
+import YouTube from 'react-youtube';
+
 const MovieShow = props => {
     const specificMovie = props.data.find(movie => movie.id === parseInt(props.match.params.id));
-    return(
-        <div className='movie-show'>
-            <img src={`https://image.tmdb.org/t/p/w500${specificMovie?.backdrop_path}`} alt={`${specificMovie?.title} backdrop poster`} />
-            <div className='movie-show-details'>
-                <h2 className='show-title'>{specificMovie?.title}</h2>
-                <p className='show-overview'>{specificMovie?.overview}</p>
-                <p className='show-released'>Released: {specificMovie?.release_date}</p>
-                <p className='show-runtime'>Runtime: {specificMovie?.runtime} minutes</p>
-                <p className='show-genres'>Genres:</p>
-                {specificMovie?.genres?.map(result => (
-                    <p>{result.name}</p>
-                ))}
+
+    const opts = {
+        // height: '100%',
+        // width: '100%',
+        playerVars: {
+            autoplay: 1
+        }
+    }
+
+    const onReady = e => {
+        e.target.pauseVideo();
+    }
+
+        return(
+        <div>
+            <div className='movie-show'>
+                <img src={`https://image.tmdb.org/t/p/w500${specificMovie?.backdrop_path}`} alt={`${specificMovie?.title} backdrop poster`} />
+                <div className='movie-show-details'>
+                    <h2 className='show-title'>{specificMovie?.title}</h2>
+                    <p className='show-overview'>{specificMovie?.overview}</p>
+                    <p className='show-released'>Released: {specificMovie?.release_date}</p>
+                    <p className='show-runtime'>Runtime: {specificMovie?.runtime} minutes</p>
+                    <div className='show-genres'>Genres:
+                        {specificMovie?.genres?.map(result => (
+                            <p>&nbsp;&nbsp;{result.name}</p>
+                        ))}
+                    </div>
+                </div>
             </div>
-            <p className='show-trailer'>Click <a href={`https://www.youtube.com/watch?v=${specificMovie?.videos?.results[0].key}`} target='_blank' rel='noreferrer'>here</a> to see the official trailer</p>
-            <p className='show-homepage'>Click <a href={specificMovie?.homepage} target='_blank' rel='noreferrer'>here</a> to go to this movie's homepage</p>
+            <div className='media-plus-link'>
+                <YouTube videoId={specificMovie?.videos.results[0].key} opts={opts} onReady={onReady} className='trailer' />
+                <p className='show-homepage'><a href={specificMovie?.homepage} target='_blank' rel='noreferrer'>{specificMovie?.title}</a>&nbsp; homepage</p>
+            </div>
         </div>
     )
 }
